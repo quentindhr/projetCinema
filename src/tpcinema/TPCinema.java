e84cc2754486949e24b4e496ae82bc29999dd036
quentindeharo <quentindeharo@localhost>
2023-02-02 20:13:40 +0100

    le message

--- src/tpcinema/TPCinema.java
+++ src/tpcinema/TPCinema.java
@@ -6,8 +6,7 @@ package tpcinema;
 public class TPCinema {
 
     
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {       
     }
     
 }
